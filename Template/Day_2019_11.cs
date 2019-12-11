179071628e8b1ff670d43bb852bebb16f89ab3ba
Johannes Salminen <johannes.salminen@liaison.com>
2019-12-11 20:16:31 +0200

    add implementation for day 11 part 1

--- Template/Day_2019_11.cs
+++ Template/Day_2019_11.cs
@@ -12,11 +12,60 @@ namespace Template
         {
             long[] intcode = input.Split(',').Select(long.Parse).ToArray();
             IntComputer ic = new IntComputer(intcode);
-            ic.inputs.Add((long)1);
+            int i = 499;
+            int j = 499;
+            int compass = 2;
+            List<Tuple<int,int>> painted = new List<Tuple<int,int>>();
+            int[,] hull = new int[1000,1000];
+            ic.inputs.Add(hull[i,j]);
             ic.compute();
-            List<long> output = ic.outputs;
-            return string.Join(',', output);
+            while (ic.isWaiting)
+            {
+                //Paint
+                //elem1: 0 = blck, 1 = wht, elem2: 0 = left, 1 = right
+                if(hull[i,j] != (int) ic.outputs.ElementAt(0))
+                {
+                    hull[i,j] = (int) ic.outputs.ElementAt(0);
+                    painted.Add(new Tuple<int, int>(i,j));
+                }
+                ic.outputs.RemoveAt(0);
+                //Rotate
+                compass = rotateCompass(compass, (int) ic.outputs.ElementAt(0));
+                ic.outputs.RemoveAt(0);
+                //Move
+                if(compass == 1)
+                    i--;
+                else if(compass == 2)
+                    j--;
+                else if(compass == 3)
+                    i++;
+                else if(compass == 4)
+                    j++;
+                ic.inputs.Add(hull[i,j]);
+                ic.compute();
+            }
+            var result = painted.GroupBy(key => key, item => 1)
+                               .Select(group => new { 
+                                   group.Key, 
+                                   Duplicates = group.Count() 
+                               }).ToList();
+
+            return result.Count.ToString();
         }
+
+        public static int rotateCompass(int compass, int direction)
+        {
+            if (direction == 0)
+                compass--;
+            else
+                compass++;
+            if (compass < 1)
+                compass = 4;
+            else if (compass > 4)
+                compass = 1;
+            return compass;
+        }
+
 
         public static string secondPuzzle(string location)
         {
@@ -34,7 +83,7 @@ namespace Template
                     }
                 }
             }
-            Tuple<int, int> winner = new Tuple<int, int>(0,0);
+            Tuple<int, int> winner = new Tuple<int, int>(0, 0);
             List<Tuple<int, int>> winnerCanSee = new List<Tuple<int, int>>();
 
             foreach (var asteroid in asteroids)
@@ -44,34 +93,36 @@ namespace Template
                 {
                     if (!asteroid.Equals(asteroid2))
                     {
-                        if(canSee.Count == 0)
+                        if (canSee.Count == 0)
                             canSee.Add(asteroid2);
-                        else {
+                        else
+                        {
                             bool sameAtan = false;
-                            foreach(var seen in canSee)
+                            foreach (var seen in canSee)
                             {
-                                if(Math.Atan2(asteroid2.Item2-asteroid.Item2,asteroid2.Item1-asteroid.Item1) == Math.Atan2(seen.Item2-asteroid.Item2,seen.Item1-asteroid.Item1)) 
+                                if (Math.Atan2(asteroid2.Item2 - asteroid.Item2, asteroid2.Item1 - asteroid.Item1) == Math.Atan2(seen.Item2 - asteroid.Item2, seen.Item1 - asteroid.Item1))
                                 {
                                     sameAtan = true;
                                     break;
                                 }
                             }
-                            if(!sameAtan)
+                            if (!sameAtan)
                             {
                                 canSee.Add(asteroid2);
                             }
                         }
                     }
                 }
-                if(winnerCanSee.Count == 0 || canSee.Count > winnerCanSee.Count) {
+                if (winnerCanSee.Count == 0 || canSee.Count > winnerCanSee.Count)
+                {
                     winner = asteroid;
                     winnerCanSee = canSee;
                 }
             }
-            
-            var sortedAsteroids = winnerCanSee.OrderBy(i => Math.Atan2(i.Item2-winner.Item2,i.Item1-winner.Item1)).ToList();
 
-            return (sortedAsteroids.ElementAt(sortedAsteroids.Count-200).Item2 * 100 + sortedAsteroids.ElementAt(sortedAsteroids.Count-200).Item1).ToString();
+            var sortedAsteroids = winnerCanSee.OrderBy(i => Math.Atan2(i.Item2 - winner.Item2, i.Item1 - winner.Item1)).ToList();
+
+            return (sortedAsteroids.ElementAt(sortedAsteroids.Count - 200).Item2 * 100 + sortedAsteroids.ElementAt(sortedAsteroids.Count - 200).Item1).ToString();
         }
     }
 }
