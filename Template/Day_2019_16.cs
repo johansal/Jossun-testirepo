60473d20e3f45e8bf368d8b41578cc87b5de964b
Johannes Salminen <johannes.salminen@liaison.com>
2019-12-19 10:05:17 +0200

    day 16 part 2 comments

--- Template/Day_2019_16.cs
+++ Template/Day_2019_16.cs
@@ -14,7 +14,7 @@ namespace Template
             //Console.WriteLine("After phase 0: " + tmp);
             for (int i = 1; i <= 100; i++)
             {
-                tmp = fftPhase(tmp,1);
+                tmp = fftPhase(tmp);
                 //Console.WriteLine("After phase " + i + ": " + tmp.Substring(0, 8));
             }
             return tmp.Substring(0, 8);
@@ -25,19 +25,19 @@ namespace Template
 
             string tmp = input;
             Console.WriteLine(tmp.Length);
-            int messageOffSet = int.Parse(tmp.Substring(0,7));
+            int messageOffSet = int.Parse(tmp.Substring(0, 7));
             for (int i = 1; i <= 100; i++)
             {
-                tmp = fftPhase(tmp,10000);
+                tmp = fftPhase(tmp);
             }
             return tmp.Substring(messageOffSet, 8);
         }
 
-        public static string fftPhase(string input, int m)
+        public static string fftPhase(string input)
         {
             int[] pattern = { 0, 1, 0, -1 };
             string output = "";
-            //use m to repeat input string
+
             for (int i = 1; i <= input.Length; i++)
             {
                 int signal = 0;
@@ -48,7 +48,7 @@ namespace Template
                         offset = offset == 3 ? 0 : offset + 1;
                     //Console.Write(offset + " (" + pattern[offset] + ") ");
                     //Console.Write(input[j-1].ToString() + "*" + pattern[offset] + " + ");
-                    signal += int.Parse(input[j-1].ToString()) * pattern[offset];
+                    signal += int.Parse(input[j - 1].ToString()) * pattern[offset];
                 }
                 signal = Math.Abs(signal);
                 //Console.Write("\n");
@@ -57,5 +57,20 @@ namespace Template
             }
             return output;
         }
+
+        /*
+        The first observation is that on row N, we skip the first N-1 elements, 
+        then sum the next N ones, skip N, subtract N-1, etc, until at the end of the row. 
+        We don't need any multiplications, just sums.
+
+        The second observation is that we can compute the partial sums of the input 
+        once at the start of one of the 100 steps, 
+        and then use cumulative_sum[k + n] - cumulative_sum[k] to compute the sum of elements 
+        from k to k + n in a single operation.
+
+        With that, the work for the last 2/3 lines needs a single operation 
+        (since the last 2/3 lines only contain a single large block of ones, see picture). 
+        The next 1/3 - 1/5 need 2 operations, the next 1/5 - 1/7 need 3, and so on.
+        */
     }
 }
