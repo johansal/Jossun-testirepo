c39ae973427ac91cad04f30ebea9c6abc5cdcaef
Johannes Salminen <jsalminen@opentext.com>
2022-04-13 11:03:33 +0300

    add authorscontroller

--- Blazor/BookStoreApp/BookStoreApp.API/Program.cs
+++ Blazor/BookStoreApp/BookStoreApp.API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connString = builder.Configuration.GetConnectionString("BookStoreAppDbConnection");
-builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer());
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer(connString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
